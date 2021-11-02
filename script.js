787c7387a4ca9f4dda103fbdc681e6058b86ca7e
Stefan Djordjevic <djordjeviczstefan@gmail.com>
2021-11-02 15:23:20 +0100

    drag and drop

--- script.js
+++ script.js
@@ -18,17 +18,58 @@ addItem.addEventListener('click',() =>{
     textPlace.style.display = "block";
     saveBtn.style.display = "block";
     addItem.style.visibility = "hidden";
-})
+});
 
 saveBtn.addEventListener('click', () =>{
     textPlace.style.display = "none";
     saveBtn.style.display = "none";
     addItem.style.visibility = "visible";
-})
-
-// function save1() {
-//     var btnSavefirst = document.getElementById('input-save-blog').value === " ";
-    
-// }
+});
 
 // Drag and Drop 
+
+const list_items = document.querySelectorAll('.item');
+const lists = document.querySelectorAll('.box-items');
+
+let draggedItem = null;
+
+for (let i = 0; i < list_items.length; i++) {
+	const item = list_items[i];
+
+	item.addEventListener('dragstart', function () {
+		draggedItem = item;
+		setTimeout(function () {
+			item.style.display = 'none';
+		}, 0)
+	});
+
+	item.addEventListener('dragend', function () {
+		setTimeout(function () {
+			draggedItem.style.display = 'block';
+			draggedItem = null;
+		}, 0);
+	})
+
+	for (let j = 0; j < lists.length; j ++) {
+		const list = lists[j];
+
+		list.addEventListener('dragover', function (e) {
+			e.preventDefault();
+		});
+		
+		list.addEventListener('dragenter', function (e) {
+			e.preventDefault();
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+		});
+
+		list.addEventListener('dragleave', function (e) {
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.0)';
+		});
+
+		list.addEventListener('drop', function (e) {
+			console.log('drop');
+			this.append(draggedItem);
+			this.style.backgroundColor = 'rgba(0, 0, 0, 0.0)';
+		});
+	}
+}
