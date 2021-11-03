33867b0f6ae99171fbc41730f4a39bd241070ecb
Oliver <oliver.vukovic@quantoxtechnology.com>
2021-11-03 11:50:04 +0100

    modal

--- script.js
+++ script.js
@@ -61,9 +61,63 @@ function getDragAfterElement(container, y) {
     const box = child.getBoundingClientRect()
     const offset = y - box.top - box.height / 2
     if (offset < 0 && offset > closest.offset) {
-      return { offset: offset, element: child }
+      return {offset: offset, element: child}
     } else {
       return closest
     }
-  }, { offset: Number.NEGATIVE_INFINITY }).element
+  }, {offset: Number.NEGATIVE_INFINITY}).element
 }
+
+
+
+//
+// function myDbl() {
+//   document.getElementById("modal").style.display = "flex";
+// }
+//
+
+
+
+//-- open modal with dbl click and change a title --//
+const changeTitle = document.querySelectorAll('.box-title');
+const newTitle = document.querySelector(".backlog");
+const modal = document.getElementById("modal");
+changeTitle.forEach(newTitle => {
+
+newTitle.addEventListener("dblclick", e => {
+  e.preventDefault()
+  modal.style.display = "flex";
+});
+})
+
+
+var writteNew = document.getElementById("test");
+var newOne = document.getElementById("title1");
+var newTwo = document.getElementById("title2");
+var newThree = document.getElementById("title3");
+var newFour = document.getElementById("title4");
+
+writteNew.addEventListener("change", function(){
+  var title = writteNew.value;
+  newOne.textContent = title;
+  modal.style.display = "none";
+})
+
+writteNew.addEventListener("change", function(){
+  var title = writteNew.value;
+  newTwo.textContent = title;
+  modal.style.display = "none";
+})
+
+writteNew.addEventListener("change", function(){
+  var title = writteNew.value;
+  newThree.textContent = title;
+  modal.style.display = "none";
+})
+
+writteNew.addEventListener("change", function(){
+  var title = writteNew.value;
+  newFour.textContent = title;
+  modal.style.display = "none";
+})
+
