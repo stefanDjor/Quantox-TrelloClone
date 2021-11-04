dcb572a3ab78137f27628d14bc6c18da371ea97d
Stefan Djordjevic <djordjeviczstefan@gmail.com>
2021-11-04 15:41:16 +0100

    add-itemsss

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
-const addItem = document.querySelector('.icon');
-const textPlace = document.querySelector('.input-area');
-const saveBtn = document.querySelector('.save-btn');
+const addItems = document.querySelectorAll('.icon');
+// const textArea = document.querySelector('.text-area');
+const saveBtns = document.querySelectorAll('.save-btn');
+
 const infoBtn = document.querySelector(".nav-info");
 const infoPhoto = document.querySelector(".info-photo")
+const textPlace = document.querySelector('.input-area');
 
-// Info open/close
-infoBtn.addEventListener("click", event => {
-    if (infoPhoto.style.display === "none") {
-        infoPhoto.style.display = "flex";
-    } else {
-        infoPhoto.style.display = "none";
-    }
+
+// Save
+addItems.forEach(addItem => {
+  addItem.addEventListener('click', () =>{
+          addItem.style.visibility = "hidden";
+          const realTextArea = addItem.closest('.card').querySelector('.text-area');
+          realTextArea .style.display = "block";
+          console.log(realTextArea)
+          console.log(addItem)
+          console.log(addItem.closest('.card'));
+  });
 });
-
-
-addItem.addEventListener('click',() =>{
-    textPlace.style.display = "block";
-    saveBtn.style.display = "block";
-    addItem.style.visibility = "hidden";
-});
-
-saveBtn.addEventListener('click', () =>{
-    textPlace.style.display = "none";
-    saveBtn.style.display = "none";
+saveBtns.forEach(saveBtn => {
+  saveBtn.addEventListener('click', (e) =>{
+    const addItem = saveBtn.closest('.card').querySelector('.icon');
+    const realTextArea = addItem.closest('.card').querySelector('.text-area');
+    realTextArea.style.display = "none";
     addItem.style.visibility = "visible";
+    console.log(realTextArea)
+          console.log(addItem)
+          console.log(addItem.closest('.card'));
+    newItem(e)
 });
+});
+
+
+// addItems.addEventListener('click',() =>{
+//     textPlace.style.display = "block";
+//     saveBtn.style.display = "block";
+//     addItems.style.visibility = "hidden";
+// });
+// saveBtn.addEventListener('click', () =>{
+//     textPlace.style.display = "none";
+//     saveBtn.style.display = "none";
+//     addItems.style.visibility = "visible";
+// });
 
 // Drag and Drop 
 
@@ -110,50 +127,6 @@ changeTitle.forEach(newTitle => {
     }
   }
   });
-
-// newOne.addEventListener("change", e => {
-//   e.preventDefault();
-//   var title = writteNew.value;
-//   newOne.textContent = title;
-//   modal.style.display = "flex";
-// })
-// save.addEventListener('click' , function(){
-//   var title = writteNew.value;
-//       if(writteNew.value === ""){
-//         modal.style.display = 'none';
-//       }
-//       else {
-//         newOne.textContent = title;
-//         modal.style.display = "none";
-//         writteNew.value = '';
-//       }  
-// });
-// window.onclick = function(event){
-//   if(event.target == modal){
-//     modal.style.display = 'none';
-//     event.preventDefault();
-//   }
-// }
-
-
-// writteNew.addEventListener("change", function(){
-//   var title = writteNew.value;
-//   newTwo.textContent = title;
-//   modal.style.display = "none";
-// })
-
-// writteNew.addEventListener("change", function(){
-//   var title = writteNew.value;
-//   newThree.textContent = title;
-//   modal.style.display = "none";
-// })
-
-// writteNew.addEventListener("change", function(){
-//   var title = writteNew.value;
-//   newFour.textContent = title;
-//   modal.style.display = "none";
-// })
-
 // ADDD NEW CARD
 const createBtn = document.getElementById('createbtn');
 const containerCards = document.querySelector('.cards');
@@ -180,15 +153,16 @@ function AddNew(){
                 </div>
                 <div class="icon-save">
                     <div class="icon">
-                        + Add items
+                            + Add items
+                        </div>
                     </div>
-                    <div class="save">
-                        <button  onclick="save1()" class="save-btn" type="button">Save</button>
+    
+                    <div class="text-area">
+                            <div class="save">
+                                <button  class="save-btn" type="button">Save</button>
+                            </div>
+                            <input id="input-save-backlog" value="" class="input-area" type="text" placeholder="Enter title for this card...">
                     </div>
-                </div>
-                <div class="text-area">
-                    <input id="input-save-backlog" value="" class="input-area" type="text" placeholder="Enter title for this card...">
-                </div>
         `
         let colors = ['#FF69B4', '#FFD700', '#CD5C5C', '#87CEFA'];
         let random_color = colors[Math.floor(Math.random() * colors.length)];
@@ -225,18 +199,19 @@ function AddNew(){
 
 
 // ADD Items
-saveBtn.addEventListener('click', newItem);
+// saveBtns.addEventListener('click', newItem);
 
-function newItem () {
-  const containers = this.parentElement.parentElement.parentElement.childNodes[3];
-  if (textPlace.value != ''){
+function newItem (e) {
+  const containers = e.target.closest('.card').querySelector('.box-items');
+  console.log(containers)
+  const realTextPlace = e.target.closest('.text-area').querySelector('input')
+  if (realTextPlace.value != ''){
       const newItem = document.createElement('div');
       newItem.classList.add('item');
       newItem.setAttribute('draggable', "true");
-      newItem.innerHTML = textPlace.value;
+      newItem.innerHTML = realTextPlace.value;
       containers.appendChild(newItem);
-      textPlace.value = '';
-
+      realTextPlace.value = '';
       newItem.addEventListener('dragstart', () => {
         newItem.classList.add('dragging')
       });
@@ -246,10 +221,15 @@ function newItem () {
         });
 
   }
-  saveBtn.style.display = "none";
-  
-  
-  
-
-  
+  // saveBtn.style.display = "none";
+ 
 }
+
+// Info open/close
+infoBtn.addEventListener("click", event => {
+  if (infoPhoto.style.display === "none") {
+      infoPhoto.style.display = "flex";
+  } else {
+      infoPhoto.style.display = "none";
+  }
+});
